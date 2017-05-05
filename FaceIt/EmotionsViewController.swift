94b433e166c48692b900067634e53fe4e0ea7385
Chase McElroy <ichasem@gmail.com>
2017-05-05 16:35:27 -0700

    set up split screen and nav VC, imported logging vc to show when functions are running in the VC

---
+++ FaceIt/EmotionsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  EmotionsViewController.swift
+//  FaceIt
+//
+//  Created by Chase McElroy on 5/5/17.
+//  Copyright © 2017 Chase McElroy. All rights reserved.
+//
+
+import UIKit
+
+class EmotionsViewController: VCLLoggingViewController {
+
+
+    // MARK: - Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        var destinationViewController = segue.destination
+        if let navigationController = destinationViewController as? UINavigationController {
+            destinationViewController = navigationController.visibleViewController ?? destinationViewController
+        }
+        if let faceViewController = destinationViewController as? FaceViewController, let identifier = segue.identifier,
+            let expression = emotionalFaces[identifier] {
+            faceViewController.expression = expression
+            faceViewController.navigationItem.title = (sender as? UIButton)?.currentTitle
+        
+        }
+    }
+    
+    private let emotionalFaces: Dictionary<String,FacialExpression> = [
+        "sad" : FacialExpression(eyes: .closed, mouth: .frown),
+        "happy" : FacialExpression(eyes: .open, mouth: .smile),
+        "worried" : FacialExpression(eyes: .open, mouth: .smirk)
+    ]
+
+}
