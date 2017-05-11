41fd50aad0da6ba2a4d65ab61db7be196538c72c
Chase McElroy <ichasem@gmail.com>
2017-05-11 13:09:53 -0700

    added some basic animations with Timer

--- FaceIt/EmotionsViewController.swift
+++ FaceIt/EmotionsViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class EmotionsViewController: VCLLoggingViewController {
+class EmotionsViewController: UIViewController {
 
 
     // MARK: - Navigation
--- FaceIt/FaceViewController.swift
+++ FaceIt/FaceViewController.swift
@@ -8,16 +8,16 @@
 
 import UIKit
 
-class FaceViewController: VCLLoggingViewController {
-
+class FaceViewController: UIViewController {
+    
     @IBOutlet weak var faceView: FaceView! {
         didSet {
             let handler = #selector(FaceView.changeScale(byReactingTo:))
             let pinchRecognizer = UIPinchGestureRecognizer (target: faceView, action: handler)
             faceView.addGestureRecognizer(pinchRecognizer)
-            let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(toggleEyes(byRactingTo:)))
-            tapRecognizer.numberOfTapsRequired = 1
-            faceView.addGestureRecognizer(tapRecognizer)
+//            let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(toggleEyes(byRactingTo:)))
+//            tapRecognizer.numberOfTapsRequired = 1
+//            faceView.addGestureRecognizer(tapRecognizer)
             let swipeUpRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(increaseHappieness))
             swipeUpRecognizer.direction = .up
             faceView.addGestureRecognizer(swipeUpRecognizer)
@@ -26,6 +26,41 @@ class FaceViewController: VCLLoggingViewController {
             faceView.addGestureRecognizer(swipeDownRecognizer)
             updateUI()
         }
+    }
+    
+    private struct HeadShake {
+        static let angle = CGFloat.pi/6                 //radians
+        static let segmentDuration: TimeInterval = 0.5  // each head shake has 3 segments
+    }
+    
+    private func rotateFace(by angle: CGFloat) {
+        faceView.transform = faceView.transform.rotated(by: angle)
+    }
+    
+    private func shakeHead() {
+        UIView.animate(
+            withDuration: HeadShake.segmentDuration,
+            animations: {
+                self.rotateFace(by: HeadShake.angle)
+        }) { finished in
+            if finished {
+                UIView.animate(
+                    withDuration: HeadShake.segmentDuration,
+                    animations: {
+                        self.rotateFace(by: -HeadShake.angle*2)
+                },
+                    completion: { finished in
+                        UIView.animate(
+                            withDuration: HeadShake.segmentDuration,
+                            animations: {
+                                self.rotateFace(by: HeadShake.angle)
+                        })
+                })
+            }
+        }
+    }
+    @IBAction func shakeHead(_ sender: UITapGestureRecognizer) {
+        shakeHead()
     }
     
     func increaseHappieness() {
@@ -56,13 +91,13 @@ class FaceViewController: VCLLoggingViewController {
             faceView?.eyesOpen = false
         case .squinting:
             faceView?.eyesOpen = false
-
+            
         }
         faceView?.mouthCurvature = mouthCurvatures[expression.mouth] ??  0.0
     }
     
     private let mouthCurvatures = [FacialExpression.Mouth.grin:0.5,.frown:-1.0,.smile:1.0,.neutral:0.0,.smirk:-0.5]
-
-
+    
+    
 }
 
